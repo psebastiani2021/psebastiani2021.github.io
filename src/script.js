74f509d7c5a2c4ff9a7fe2b22563433ee7033dc5
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-25 20:12:39 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,13 +1,45 @@
+// Calculator
+const display = document.getElementById('display');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalButton = document.getElementById('equal');
+const clearButton = document.getElementById('clear');
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    display.value += button.textContent;
+  });
+});
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    display.value += button.textContent;
+  });
+});
+
+equalButton.addEventListener('click', () => {
+  try {
+    const result = eval(display.value);
+    display.value = result;
+  } catch (error) {
+    display.value = 'Syntax error';
+  }
+});
+
+clearButton.addEventListener('click', () => {
+  display.value = '';
+});
+
+const dotButton = document.querySelector(".dot");
+
+dotButton.addEventListener("click", () => {
+  if (!currentNumber.includes(".")) {
+    currentNumber += ".";
+    updateDisplay();
+  }
+});
 // Console message
 window.onload = onLoadMsg
 function onLoadMsg() {
   console.log("Student Tools is an open source project. https://github.com/psebastiani2021/psebastiani2021.github.io");
-}
-// Custom attribute
-const resetStyles = document.querySelector('a')
-resetStyles.setAttribute('data-resetStyles')
-// I want to thank KemPavel, some of their pieces of code are used here
-let nav = document.getElementById('navigation');
-function toggleMenu() {
-  nav.classList.toggle('navigation--visible');
 }
