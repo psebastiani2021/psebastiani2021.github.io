132fb783e94bdc59e114b5448e685763e93445c2
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-29 18:18:25 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,30 +1,3 @@
-// Tools
-/// Calculator
-const input = document.getElementById('input');
-const math = window.math; 
-const parser = new math.parser();
-
-input.addEventListener('input', () => {
-  const filteredValue = input.value.replace(/[^0-9+\-*/.%()\[\]{}]/g, '');
-  input.value = filteredValue;
-});
-
-const calculateButton = document.getElementById('calculate');
-const resultDiv = document.getElementById('result');
-
-calculateButton.addEventListener('click', () => {
-  const expression = input.value;
-  try {
-    const result = parser.evaluate(expression);
-    resultDiv.textContent = `${result}`;
-  } catch (error) {
-    resultDiv.textContent = 'Syntax error.';
-  }
-});
-
-function appendValue(value) {
-  document.getElementById('input').value += value;
-}
 // Console message
 window.onload = onLoadMsg
 function onLoadMsg() {
