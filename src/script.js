350f96a9cb5b733e8a9668fae610cab431f75c3c
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-07-01 20:56:46 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,28 @@
+// Calculator
+const input = document.getElementById('input');
+
+input.addEventListener('input', () => {
+  // Remove any non-numeric, non-symbol, and non-grouping characters from the input value
+  const filteredValue = input.value.replace(/[^0-9+\-*/.%()\[\]]/g, '');
+  input.value = filteredValue;
+});
+
+
+const calculateButton = document.getElementById('calculate');
+const resultDiv = document.getElementById('result');
+
+calculateButton.addEventListener('click', () => {
+  const expression = input.value;
+  try {
+    const result = eval(expression);
+    resultDiv.textContent = `${result}`;
+  } catch (error) {
+    resultDiv.textContent = 'Syntax error.';
+  }
+});
+function appendValue(value) {
+  document.getElementById('input').value += value;
+}
 // Console message
 window.onload = onLoadMsg
 function onLoadMsg() {
